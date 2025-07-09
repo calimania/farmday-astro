40050c287456962a75cebdbcdf65a64d27911c2a
Daveed <daveed@santocabron.com>
2025-07-09 16:10:33 -0400

    using new farmday domains

--- src/config.ts
+++ src/config.ts
@@ -1,20 +1,20 @@
 export const markketplace = {
-  api: import.meta.env.STRAPI_URL || 'https://api.markket.place',
-  markket: import.meta.env.MARKKET_URL || 'https://de.markket.place',
+  api: import.meta.env.STRAPI_URL || 'https://markket.farmday.io',
+  markket: import.meta.env.MARKKET_URL || 'https://api.farmday.io',
   store_slug: import.meta.env.STORE_SLUG || 'farmday',
-  markketplace: import.meta.env.MARKKETPLACE_URL || 'https://dev.markket.place',
+  markketplace: import.meta.env.MARKKETPLACE_URL || 'https://de.markket.place',
   posthog_id: import.meta.env.PUBLIC_POSTHOG_KEY || '',
-  url: import.meta.env.PUBLIC_URL || 'https://farmday.caliman.org',
+  url: import.meta.env.PUBLIC_URL || 'https://farmday.io',
   content: {
-    title: 'Markketplace',
-    url: 'https://dev.markket.place',
+    title: 'Farmday',
+    url: 'https://farmday.io',
   },
   styles: {
     cover: 'https://markketplace.nyc3.digitaloceanspaces.com/uploads/c2491ef7c413165be47c9882a08d7ffd.png',
     secondary_color: '',
     primary_color: '',
-    logo: 'https://markketplace.nyc3.digitaloceanspaces.com/uploads/e9f3a0b16c347ff596e4d0a5c7b0d47d.png',
+    logo: 'https://markketplace.nyc3.digitaloceanspaces.com/uploads/de9a4e973e95821a8fa312cdb0f79db4.png',
     color: '',
-    favicon: 'https://markketplace.nyc3.digitaloceanspaces.com/uploads/7e7d574c6559b065e8fcf0975792a22a.png',
+    favicon: 'https://markketplace.nyc3.digitaloceanspaces.com/uploads/dcc2b867fb4736c356bb481fc6eecab5.png',
   }
 };
